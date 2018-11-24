ba2477eb5704336759f41c65e8798209fbd32b55
Kai Bepperling <kai.bepperling@gmail.com>
2018-11-24 19:39:47 +0100

    Added a how to setuo guide for the domoticz stuff

--- domoticz_events/changeCounter.lua
+++ domoticz_events/changeCounter.lua
@@ -1,3 +1,24 @@
+--[[
+    How To setup guide:
+    To use this dzVents-event you should use at least a domoticz v4.9xxx
+    First you need to rename the subsensors of roode to the names you see in the script. 
+    Domoticz is not able to pass the subsensor names correctly so this is a manual step.
+    1. Go to Setup -> Hardware -> Devices
+    2. Search for MySensors
+    3. Make sure you have exactly one person inside the room (counter is set to 1)
+    4. Rename:
+        4.1. "Text"-Sensor witht the value 1 to "PeopleCounter"
+        4.2. "AC"-Sensor to any Room Name
+        4.4. "Text"-Sensor witht the higher threshold value to "Threshold"
+    5. Add all these devices
+    6. Go to Setup -> Hardware
+    7. Add "Dummy"-Hardware named Roode
+    8. Add two "Light/Switch"-devices named "ChangeCounter" and "Recalibrated"
+    9. Change ChangeCounter to a selector switch with 4 levels (Off 1,2,3,4) or as many people you want to set
+    10. Change Recalibrate to a "Push on Button" device
+        10.1 Set the "On Action": http://<IP:Port>/json.htm?type=command&param=udevice&idx=<ID of Threhsold-device>&nvalue=0&svalue=recalibrate 
+    11. Check that the names are matching the variable names of the script below
+--]]
 local ChangeCounter = 'ChangeCounter'
 local lastCounter 
 return {
