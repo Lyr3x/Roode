592985d13ba3b97b70b746692b0ee169559b6284
Kai Bepperling <kai.bepperling@gmail.com>
2018-11-24 19:18:12 +0100

    Addode complete version changelog

--- README.md
+++ README.md
@@ -18,7 +18,15 @@ Repository initialized with version 0.9.4 and MySensors 2.3.0-alpha
 * Case (see .stl files) -> coming soon
 
 ## Changelog
-### v0.9.4
-* some minor bug fixes
-* improved stability
-* added standard deviation threshold calculation
+### Changelog v0.9.4-alpha2:
+* added incomplete push button support
+    * There is a need of 2 spare interrupt PINS
+    * May be working with PinChangeInterrupt library
+* minor bugfixes
+* rename send() function and changed signature
+### Changelog v0.9.4-beta:
+* changed IR-calibration hardcoded value to THRESHOLD_X 
+* CALIBRATION_VAL to 4000
+###Changelog v.0.9.4-release
+* Added standard deviation threhsold calculation
+* Removed constant THRESHOLD_X
--- roode/Configuration.h
+++ roode/Configuration.h
@@ -6,15 +6,18 @@ Be carfeul with reconfiguring!
 /*
   RooDe version number
   Please update frequently
-  Changelog v0.9.4-alpha2:
-  - added incomplete push button support
-    - There is a need of 2 spare interrupt PINS
-    - May be working with PinChangeInterrupt library
-  - minor bugfixes
-  - rename send() function and changed signature
-  Changelog v0.9.4-beta:
-  - changed IR-calibration hardcoded value to THRESHOLD_X 
-  - CALIBRATION_VAL to 4000
+### Changelog v0.9.4-alpha2:
+* added incomplete push button support
+    * There is a need of 2 spare interrupt PINS
+    * May be working with PinChangeInterrupt library
+* minor bugfixes
+* rename send() function and changed signature
+### Changelog v0.9.4-beta:
+* changed IR-calibration hardcoded value to THRESHOLD_X 
+* CALIBRATION_VAL to 4000
+###Changelog v.0.9.4-release
+* Added standard deviation threhsold calculation
+* Removed constant THRESHOLD_X
 */
 #define ROODE_VERSION "0.9.4"
 
