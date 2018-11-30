9971fb8cd3fc3dd395d5097d320900c1b491b381
Kai Bepperling <kai.bepperling@gmail.com>
2018-11-30 02:30:26 +0100

    Cleared old PeopleCounter stuff

--- lib/PeopleCounter/PeopleCounter.h
+++ lib/PeopleCounter/PeopleCounter.h
@@ -1,28 +1,5 @@
 #ifndef PeopleCounter_h
 #define PeopleCounter_h
 
-#include "core/MySensorsCore.h"
-#include "SPI.h" 
-#include "Arduino.h"
-class PeopleCounter
-{
-  public:
-    PeopleCounter(int corridor_enable, int corridor_analog, int room_enable, int room_analog);
-    ~PeopleCounter();
-    void Setup(int threshold);
-    void Presentation();
-    int checkSensors();
-    void setTo(int high_low);
-  private:
-    int calibration();
-    int ROOM_ANALOG;
-    int CORRIDOR_ANALOG;
-    int ROOM_ENABLE;
-    int CORRIDOR_ENABLE;
-    int _threshold;
-    int inout;
-    int irrVal;
-    int ircVal;
-};
 
 #endif
