1c091f3f89e9782d7ec8d7b9eabfe29330da5431
Kai Bepperling <kai.bepperling@gmail.com>
2018-12-22 10:15:00 +0100

    typo in error message

--- lib/Communication/Communication.cpp
+++ lib/Communication/Communication.cpp
@@ -7,7 +7,7 @@ void reportToController(int threshold = 0, int errorcode = -1)
     if (threshold > 8000)
     {
         String errorMsg = "Error code: " + errorcode;
-        errorMsg += " , Out of rang";
+        errorMsg += " , Out of range";
         send(thrMsg.set(errorMsg));
     }
 }
