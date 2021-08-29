d2ac7104a7f72c8a530b52f07888726a14c8ca70
Kai Bepperling <kai.bepperling@gmail.com>
2021-08-29 16:32:13 +0200

    Fix version sensor

--- lib/Configuration/Config.h
+++ lib/Configuration/Config.h
@@ -6,38 +6,7 @@ License: GPLv3
 #define CONFIG_H
 #include <Wire.h>
 #include <Arduino.h>
-/* RooDe Configuration file
-The predefined config enables most of the features and uses the NRF24L01+ Radio module
-Be carfeul with reconfiguring! Some options shouldnt be changed!
-*/
 
-/*
-  RooDe version number
-  Please update frequently
-### Changelog v1.0:
-* Complete architecture refactor
-  * New abstraction layers:
-    * Sensor
-    * Transmitter
-  * Easy to extend with the type of Sensors and Transmitters
-* Support for ESP8266 and MQTT
-* Support for Arduino with MySensors
-* Removed SHARP_IR infrared Sensors
-* Changed Message types
-* Added new message type INFO 
-* Independent sensor calibration
-* added full VL53L0X support
-  * measruing speed improvements 
-  * Fixed LONG_RANGE mode which gave just -1 as result
-  * Fixed receiving and sending message issues
-  * Added OLED brightness config option
-  * general bug fixes and improvements
-* added untested VL53L1X support
-*/
-#define ROODE_VERSION "1.1-beta"
-/*
-###### FEATURE SELECTION ######
-*/
 #define USE_VL53L1X
 // #define USE_OLED // Activates OLED 128x32 support including brightness control.
 #define CALIBRATIONV2
