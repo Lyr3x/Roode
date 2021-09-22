89f49a8798d893fd798bc35b5e06509c81d8181e
Kai Bepperling <kai.bepperling@gmail.com>
2021-09-22 13:52:01 +0200

    Clean up files

--- components/roode/__init__.py
+++ components/roode/__init__.py
@@ -12,10 +12,8 @@ CONF_ROODE_ID = "roode_id"
 roode_ns = cg.esphome_ns.namespace("roode")
 Roode = roode_ns.class_("Roode", cg.PollingComponent, i2c.I2CDevice)
 
-CONF_OPTICAL_CENTER = 'optical_center'
 CONF_ROI_HEIGHT= 'roi_height'
 CONF_ROI_WIDTH = 'roi_width'
-CONF_DIST_THRESHOLD = 'distance_threshold'
 CONF_CALIBRATION = "calibration"
 CONF_ROI_CALIBRATION = "roi_calibration"
 CONF_INVERT_DIRECTION = "invert_direction"
@@ -23,7 +21,6 @@ CONF_THRESHOLD_PERCENTAGE = "threshold_percentage"
 CONF_RESTORE_VALUES = "restore_values"
 SETTERS = {
     CONF_THRESHOLD_PERCENTAGE: "set_threshold_percentage",
-    CONF_OPTICAL_CENTER: 'set_optical_center',
     CONF_ROI_HEIGHT: 'set_roi_height',
     CONF_ROI_WIDTH: 'set_roi_width',
     CONF_RESTORE_VALUES: 'set_restore_values',
@@ -40,7 +37,6 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_ROI_CALIBRATION, default='false'): cv.boolean,
             cv.Optional(CONF_INVERT_DIRECTION, default='false'): cv.boolean,
             cv.Optional(CONF_RESTORE_VALUES, default='false'): cv.boolean,
-            cv.Optional(CONF_DIST_THRESHOLD, default=[1500, 1500]): cv.All([cv.int_range(min=0, max=0xFFFF, max_included=False)], cv.Length(min=2, max=2)),
         }
     )
     .extend(cv.polling_component_schema("100ms"))
