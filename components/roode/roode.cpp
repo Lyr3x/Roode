de3d18c16064d8104003b66535b0082addd0cea4
Kai Bepperling <kai.bepperling@gmail.com>
2021-12-22 18:43:20 +0100

    Fix sensor String

--- components/roode/roode.cpp
+++ components/roode/roode.cpp
@@ -89,8 +89,7 @@ namespace esphome
         }
 
         void Roode::handleSensorStatus()
-        {
-            statusString = VL53L1X::rangeStatusToString(last_sensor_status); // This function call will manipulate the range_status variable
+        {     statusString = VL53L1X::rangeStatusToString(sensor_status); // This function call will manipulate the range_status variable
             if (last_sensor_status != sensor_status && sensor_status != VL53L1X::RangeStatus::RangeValid)
             {
                 {
