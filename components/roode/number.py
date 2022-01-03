73e483883287ed64e6a0863a3643183e5d5cfc2a
Kai Bepperling <kai.bepperling@gmail.com>
2022-01-03 14:05:23 +0100

    Revert "Fix formatting"
    This reverts commit a5ef513a32d1b4c1c6310c6655964183219804b0.

--- components/roode/number.py
+++ components/roode/number.py
@@ -2,7 +2,10 @@ from typing import OrderedDict
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ICON, CONF_MAX_VALUE
+from esphome.const import (
+    CONF_ICON,
+    CONF_MAX_VALUE
+)
 from esphome.cpp_generator import MockObj
 
 from components.persisted_number import PERSISTED_NUMBER_SCHEMA, new_persisted_number
@@ -16,19 +19,20 @@ CONF_PEOPLE_COUNTER = "people_counter"
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_ROODE_ID): cv.use_id(Roode),
-        cv.Optional(CONF_PEOPLE_COUNTER): PERSISTED_NUMBER_SCHEMA.extend(
-            {
-                cv.Optional(CONF_ICON, default="mdi:counter"): cv.icon,  # new default
-                cv.Optional(CONF_MAX_VALUE, 10): cv.int_range(1, 255),
-            }
-        ),
+        cv.Optional(CONF_PEOPLE_COUNTER): PERSISTED_NUMBER_SCHEMA.extend({
+            cv.Optional(CONF_ICON, default="mdi:counter"): cv.icon,  # new default
+            cv.Optional(CONF_MAX_VALUE, 10): cv.int_range(1, 255),
+        }),
     }
 )
 
 
 async def setup_people_counter(config: OrderedDict, hub: MockObj):
     counter = await new_persisted_number(
-        config, min_value=0, step=1, max_value=config[CONF_MAX_VALUE]
+        config,
+        min_value=0,
+        step=1,
+        max_value=config[CONF_MAX_VALUE]
     )
     cg.add(hub.set_people_counter(counter))
 
