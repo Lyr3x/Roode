a5ef513a32d1b4c1c6310c6655964183219804b0
Kai Bepperling <kai.bepperling@gmail.com>
2022-01-03 13:33:12 +0100

    Fix formatting

--- components/persisted_number/__init__.py
+++ components/persisted_number/__init__.py
@@ -8,16 +8,27 @@ from esphome.const import (
     CONF_RESTORE_VALUE,
 )
 
-PersistedNumber = number.number_ns.class_("PersistedNumber", number.Number, cg.Component)
+PersistedNumber = number.number_ns.class_(
+    "PersistedNumber", number.Number, cg.Component
+)
 
-PERSISTED_NUMBER_SCHEMA = number.NUMBER_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(PersistedNumber),
-    cv.Optional(CONF_RESTORE_VALUE, default=True): cv.boolean,
-})
+PERSISTED_NUMBER_SCHEMA = number.NUMBER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(PersistedNumber),
+        cv.Optional(CONF_RESTORE_VALUE, default=True): cv.boolean,
+    }
+)
 
 
-async def new_persisted_number(config: OrderedDict, min_value: float, max_value: float, step: Optional[float] = None):
-    var = await number.new_number(config, min_value=min_value, max_value=max_value, step=step)
+async def new_persisted_number(
+    config: OrderedDict,
+    min_value: float,
+    max_value: float,
+    step: Optional[float] = None,
+):
+    var = await number.new_number(
+        config, min_value=min_value, max_value=max_value, step=step
+    )
     await cg.register_component(var, config)
     if CONF_RESTORE_VALUE in config:
         cg.add(var.set_restore_value(config[CONF_RESTORE_VALUE]))
