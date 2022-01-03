c21aab5b708985b38c50d5abdc087cc582291bb7
Carson Full <carson_full@tsco.org>
2022-01-03 07:56:12 -0600

    black format

--- components/roode/number.py
+++ components/roode/number.py
@@ -2,10 +2,7 @@ from typing import OrderedDict
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import (
-    CONF_ICON,
-    CONF_MAX_VALUE
-)
+from esphome.const import CONF_ICON, CONF_MAX_VALUE
 from esphome.cpp_generator import MockObj
 
 from ..persisted_number import PERSISTED_NUMBER_SCHEMA, new_persisted_number
@@ -19,20 +16,19 @@ CONF_PEOPLE_COUNTER = "people_counter"
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_ROODE_ID): cv.use_id(Roode),
-        cv.Optional(CONF_PEOPLE_COUNTER): PERSISTED_NUMBER_SCHEMA.extend({
-            cv.Optional(CONF_ICON, default="mdi:counter"): cv.icon,  # new default
-            cv.Optional(CONF_MAX_VALUE, 10): cv.int_range(1, 255),
-        }),
+        cv.Optional(CONF_PEOPLE_COUNTER): PERSISTED_NUMBER_SCHEMA.extend(
+            {
+                cv.Optional(CONF_ICON, default="mdi:counter"): cv.icon,  # new default
+                cv.Optional(CONF_MAX_VALUE, 10): cv.int_range(1, 255),
+            }
+        ),
     }
 )
 
 
 async def setup_people_counter(config: OrderedDict, hub: MockObj):
     counter = await new_persisted_number(
-        config,
-        min_value=0,
-        step=1,
-        max_value=config[CONF_MAX_VALUE]
+        config, min_value=0, step=1, max_value=config[CONF_MAX_VALUE]
     )
     cg.add(hub.set_people_counter(counter))
 
